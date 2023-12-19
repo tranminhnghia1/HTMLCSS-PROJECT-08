b0b7d01e933252332c54592a1ddb04a86ab72a9d
tranminhnghia1 <nghia101022@gmail.com>
2023-12-20 01:38:09 +0700

    update dropdown

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,147 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+
+
+/**
+ * Hàm tải template
+ *
+ * Cách dùng:
+ * <div id="parent"></div>
+ * <script>
+ *  load("#parent", "./path-to-template.html");
+ * </script>
+ */
 function load(selector, path) {
   const cached = localStorage.getItem(path);
   if (cached) {
-    document.querySelector(selector).innerHTML = cached;
+    $(selector).innerHTML = cached;
   }
 
   fetch(path)
     .then((res) => res.text())
     .then((html) => {
       if (html !== cached) {
-        document.querySelector(selector).innerHTML = html;
+        $(selector).innerHTML = html;
         localStorage.setItem(path, html);
       }
+    })
+    .finally(() => {
+      window.dispatchEvent(new Event("template-loaded"));
     });
+}
+
+
+/**
+ * Hàm kiểm tra một phần tử
+ * có bị ẩn bởi display: none không
+ */
+function isHidden(element) {
+  if (!element) return true;
+
+  if (window.getComputedStyle(element).display === "none") {
+    return true;
+  }
+
+  let parent = element.parentElement;
+  while (parent) {
+    if (window.getComputedStyle(parent).display === "none") {
+      return true;
+    }
+    parent = parent.parentElement;
+  }
+
+  return false;
+}
+
+/**
+* Hàm buộc một hành động phải đợi
+* sau một khoảng thời gian mới được thực thi
+*/
+function debounce(func, timeout = 300) {
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func.apply(this, args);
+    }, timeout);
+  };
+}
+
+//dropdown
+/**
+ * Hàm tính toán vị trí arrow cho dropdown
+ *
+ * Cách dùng:
+ * 1. Thêm class "js-dropdown-list" vào thẻ ul cấp 1
+ * 2. CSS "left" cho arrow qua biến "--arrow-left-pos"
+ */
+const calArrowPos = debounce(() => {
+  if (isHidden($(".js-dropdown-list"))) return;
+
+  const items = $$(".js-dropdown-list > li");
+
+  items.forEach((item) => {
+    const arrowPos = item.offsetLeft + item.offsetWidth / 2;
+    item.style.setProperty("--arrow-left-pos", `${arrowPos}px`);
+  });
+});
+
+// Tính toán lại vị trí arrow khi resize trình duyệt
+window.addEventListener("resize", calArrowPos);
+
+// Tính toán lại vị trí arrow sau khi tải template
+window.addEventListener("template-loaded", calArrowPos);
+
+
+//hove dropdown con
+/**
+/**
+ * Giữ active menu khi hover
+ *
+ * Cách dùng:
+ * 1. Thêm class "js-menu-list" vào thẻ ul menu chính
+ * 2. Thêm class "js-dropdown" vào class "dropdown" hiện tại
+ *  nếu muốn reset lại item active khi ẩn menu
+ */
+window.addEventListener("template-loaded", handleActiveMenu);
+
+function handleActiveMenu() {
+  const dropdowns = $$(".js-dropdown");
+  const menus = $$(".js-menu-list");
+  const activeClass = "menu-column__item--active";
+
+  const removeActive = (menu) => {
+    menu.querySelector(`.${activeClass}`)?.classList.remove(activeClass);
+  };
+
+  const init = () => {
+    menus.forEach((menu) => {
+      const items = menu.children;
+      if (!items.length) return;
+
+      removeActive(menu);
+      if (window.innerWidth > 991) items[0].classList.add(activeClass);
+
+      Array.from(items).forEach((item) => {
+        item.onmouseenter = () => {
+          if (window.innerWidth <= 991) return;
+          removeActive(menu);
+          item.classList.add(activeClass);
+        };
+        item.onclick = () => {
+          if (window.innerWidth > 991) return;
+          removeActive(menu);
+          item.classList.add(activeClass);
+          item.scrollIntoView();
+        };
+      });
+    });
+  };
+
+  init();
+
+  dropdowns.forEach((dropdown) => {
+    dropdown.onmouseleave = () => init();
+  });
 }
